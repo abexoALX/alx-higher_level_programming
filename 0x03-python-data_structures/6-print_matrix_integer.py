a2a9860fd45e8a0066b76a5ee96c260963a9f93e
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-08-28 02:38:00 -0700

    asqwes

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
         for col in range(len(line)):
-            print("{:d}".format(line[col]), end="" if col == len(line) - 1 else " ")
+            print(
+                    "{:d}".format(line[col]), 
+                    end="" if col == len(line) - 1 else " ")
         print("")
