353099598e6898773560eeaf4d6599de8b6ff5ea
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-08-28 03:48:30 -0700

    asqwe

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,12 +2,5 @@
 def divisible_by_2(my_list=[]):
     if not my_list:
         return
-    index = 0
-    for i in my_list:
-        if i % 2 == 0:
-            new_list[index] = True
-        else:
-            new_list[index] = False
-        index = index + 1
+    new_list = [n % 2 == 0 for n in my_list]
     return new_list
-
