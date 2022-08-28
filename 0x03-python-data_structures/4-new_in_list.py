0742d9914cc6d0a657fce575a759b672b4046887
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-08-28 01:45:30 -0700

    asqwesla

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if not my_list:
+        return
+    new_list = my_list.copy()
+    if(idx >=0 and idx < len(my_list)):
+        new_list[idx] = element
+        return new_list
+    else:
+        return my_list
