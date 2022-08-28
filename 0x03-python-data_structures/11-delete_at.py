a64bc1286674fbcc3e4b0154e922247d0e9897d6
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-08-28 05:43:30 -0700

    asqwe

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if not my_list:
-        return
-    if idx < 0 or idx > len(my_list):
-        return my_list
-    else:
+    if my_list is not None and 0 <= idx < len(my_list):
         del my_list[idx]
-        return mylist
+    return my_list
