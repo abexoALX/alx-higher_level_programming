852850055065b8c9038be0174eeaf79e37465bd0
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-09-12 04:17:45 -0700

    third

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    val = 0
+    for index in range(x):
+        try:
+            print('{:d}'.format(my_list[index]), end='')
+            val += 1
+        except IndexError:
+            break
+        except Exception:
+            pass
+    print('')
+    return val
