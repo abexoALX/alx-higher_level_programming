f5164cb821f96b9b0b3a65f9e51df1621115800e
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-09-13 03:48:55 -0700

    exception in the class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,5 +11,5 @@ class Square:
         if type(size) != int:
             raise TypeError('size must be an integer')
         elif size < 0:
-             raise ValueError('size must be >= 0')
+            raise ValueError('size must be >= 0')
         self.__size = size
