2fc28658523672f5fb5c045d973a1582419b3cfe
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-09-13 03:30:26 -0700

    define area and return the value in class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class and initialise its size
+Its implements value and type checks for its attributes
+"""
+class Square:
+    """Square implementation
+    """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+    def area(self):
+        area = self.__size * self.__size
+        return area
