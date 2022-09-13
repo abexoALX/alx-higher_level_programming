7aab71b36b2b530a26d08b750197b60738fefb76
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-09-13 03:21:36 -0700

    exception in the class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class and initialise its size
+Its implements value and type checks for its attributes
+"""
+class Square:
+    """Square implementation
+    """
+    def __init__(self, size = 0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+             raise ValueError('size must be >= 0')
+        self.__size = size
