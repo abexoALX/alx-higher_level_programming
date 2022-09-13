989f754bd78916ceff6a480b6f0649f035ca238e
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-09-13 03:44:53 -0700

    with private instance attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""
-This module defines a Square class and initialise its size
-"""
+"""This module defines a Square class and initialise its size"""
+
+
 class Square:
-    """Square implementation
-    """
+    """Square implementation"""
     def __init__(self, size):
         self.__size = size
