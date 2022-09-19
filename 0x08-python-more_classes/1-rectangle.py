2420ad6b3873cfcae509c47816d599bd8a994f74
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-09-19 10:02:20 -0700

    second;

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,8 +4,8 @@
 
 class Rectangle:
     """Retangle object with getter and setters"""
-
     
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -21,18 +21,18 @@ class Rectangle:
         elif value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
+    
+    @property
+    def height(self):
+        return self.__height
 
-     @property
-     def height(self):
-         return self.__height
-
-     @height.setter
-     def height(self, value):
-         if not isinstance(value, int):
-             raise TypeError('height must be an integer')
-         elif value < 0:
-             raise ValueError('height must be >= 0')
-         self.__height = value
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
 
 
 
