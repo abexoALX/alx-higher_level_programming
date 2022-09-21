e0e82d6bdf2c9033c22a46685f4fca344e751f19
abexoALX <abenezer.ashenafi.alx@gmail.com>
2022-09-21 19:04:45 +0300

    jsnf

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """
-This module defines `print_square`
-The function prints a square
+This module defines `say_my_name`
+The function prints the full name using first_name and last_name
 """
 
 
-def print_square(size):
-        """prints a square with size, `size`
-            Args:
-                    size (int)
-                        Raises:
-                                TypeError: size must be an integer
-                                        ValueError: size must be >= 0
-                                            """
+def say_my_name(first_name, last_name=""):
+    """prints a name using first_name and last_name
+    Args:
+        first_name (str)
+        last_name (str, optional). Defaults to "".
+    Raises:
+        TypeError: first_name must be a string
+        TypeError: last_name must be a string
+    """
 
-                                                if type(size) != int:
-                                                            raise TypeError('size must be an integer')
-                                                            elif size < 0:
-                                                                        raise ValueError('size must be >= 0')
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    elif not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
 
-                                                                        for x in range(size):
-                                                                                    print('#' * size)
+    print('My name is {} {}'.format(first_name, last_name))
