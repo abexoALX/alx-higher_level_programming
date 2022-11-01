f85845555f006b491d0f9ba33f553cbf5f590ac4
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-11-01 09:20:44 -0700

    no null value

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- script that lists all records of the table second_table
-SELECT score, name FROM second_table ORDER BY score DESC 
+SELECT score, name FROM second_table WHERE name  IS NOT NULL ORDER BY score DESC 
