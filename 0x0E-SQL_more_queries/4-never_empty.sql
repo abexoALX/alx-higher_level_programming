f751f0c17aab33cd3c7354663784da4679a20da6
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-11-02 07:43:22 -0700

    create

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- script that creates the table id_not_null
-CREATE TABLE IF NOT EXISTS hbtn_0d_2.id_not_null (id INT NOT NULL DEFAULT '1', name VARCHAR(256)); 
+CREATE TABLE IF NOT EXISTS hbtn_0d_2.id_not_null (id INT DEFAULT '1' NOT NULL, name VARCHAR(256)); 
