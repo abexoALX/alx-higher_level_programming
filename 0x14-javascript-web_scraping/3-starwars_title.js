011c297ae004c0ef371a67ca5dd2247007429123
abeni <abenezer.ashenafi.alx@gmail.com>
2023-01-23 15:51:05 +0300

    final

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,19 +1,16 @@
 #!/usr/bin/node
-/**
- * Script that prints the title of a Star Wars movie
- * where the episode number matches a given integer
- */
+
 const request = require('request');
-const process = require('process');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-const episodNumber = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${episodNumber}`;
-
-request.get(url, (err, _resp, body) => {
-    if (err === null) {
-        const data = JSON.parse(body);
-        console.log(data.title);
-    } else {
-        console.log(err);
-    }
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
 });
