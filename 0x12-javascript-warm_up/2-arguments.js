f9190f04eda08fe9b4f91988b675a0d5e31e267b
abeni <abenezer.ashenafi.alx@gmail.com>
2023-02-14 09:41:02 +0300

    general

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,3 @@
 #!/usr/bin/node
-const { argv } = require('process');
-
-const argvLength = argv.length - 2;
-if (argvLength <= 0) {
-  console.log('No argument');
-} else if (argvLength === 1) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
